2ac710d8dfa788c50d520d404196e86c8881511b
jwookie2 <jwookie2@hotmail.com>
2025-07-10 19:40:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 
 import streamlit as st
 import pandas as pd
-#from premium.upgrade_panel import upgrade_panel
+from premium.upgrade_panel import upgrade_panel
 from premium.tier_manager import is_premium, get_user_credit, add_credits
 from ranking.seo_vector_ranker import rank_repos
 from premium.tier_manager import is_premium, get_user_credit, add_credits, get_user_tier #from premium.tier_manager import is_premium, get_user_tier, get_credits, add_credits
