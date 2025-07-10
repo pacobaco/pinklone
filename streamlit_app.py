5a3fa9570a4297825b94d697688d97117736c06f
jwookie2 <jwookie2@hotmail.com>
2025-07-10 17:31:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@
 import streamlit as st
 import pandas as pd
 #from premium.upgrade_panel import upgrade_panel
-
+from premium.tier_manager import is_premium, get_user_credit, add_credits
 from ranking.seo_vector_ranker import rank_repos
 from premium.upgrade_panel import upgrade_panel
-from premium.tier_manager import is_premium, get_user_tier, get_credits, add_credits
+#from premium.tier_manager import is_premium, get_user_tier, get_credits, add_credits
 from deploy.fork_repo import fork_repo
 from dao.proposals import submit_proposal, get_proposals
 from social.comments import get_comments, add_comment
